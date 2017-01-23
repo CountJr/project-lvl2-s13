d24dc759815d6debdf719b0069359dcc9960930b
Kirill Grafov <countjr@gmail.com>
2017-01-23 06:32:44 -0600

    fix command line arguments

--- __tests__/flatCompareTest.js
+++ __tests__/flatCompareTest.js
@@ -16,12 +16,12 @@ const outputFile = {
 
 // statuses: unchanged, changed, new, deleted.
 const resultOutput = [
-  { key: 'host', status: 'unchanged', oldValue: 'hexlet.io', newValue: 'hexlet.io' },
-  { key: 'timeout', status: 'changed', oldValue: '50', newValue: '20' },
-  { key: 'proxy', status: 'deleted', oldValue: '123.234.53.22', newValue: '' },
-  { key: 'verbose', status: 'new', oldValue: '', newValue: 'true' },
+  { key: 'host', parent: '', status: 'unchanged', oldValue: 'hexlet.io', newValue: 'hexlet.io' },
+  { key: 'timeout', parent: '', status: 'changed', oldValue: '50', newValue: '20' },
+  { key: 'proxy', parent: '', status: 'deleted', oldValue: '123.234.53.22', newValue: '' },
+  { key: 'verbose', parent: '', status: 'new', oldValue: '', newValue: 'true' },
 ];
 
 test('compare json files', () => {
-  //expect(compare()).toEqual(resultOutput);
+  // expect(compare()).toEqual(resultOutput);
 });
--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,12 +3,21 @@
 // @flow
 
 import commander from 'commander';
+import print from 'mylittlecolor';
 
-commander.version('0.1.0')
-  .usage('gendiff [options] <first_config> <second_config>>\n'
-    + '\n'
-    + 'Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'Output format')
+commander
+  .version('0.1.0')
+  .option('-f, --format [type]', 'Output format');
+
+
+commander
+  .arguments('<first_config> <second_config>')
+  .action((firstConfig, secondConfig) => {
+    // passing arguments
+    print(`#red{${firstConfig}} #green{${secondConfig}}`);
+  });
+
+commander
   .parse(process.argv);
 
 if (!commander.args.length) commander.help();
