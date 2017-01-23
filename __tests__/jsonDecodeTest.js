5269e488207064b2ae03a53f776122605ae409dc
Kirill Grafov <countjr@gmail.com>
2017-01-23 07:18:35 -0600

    fix command line arguments and make first test

---
+++ __tests__/jsonDecodeTest.js
@@ -0,0 +1,19 @@
+// @flow
+
+// import {  } from 'module';
+
+const json = '{"one": "valone", "two": "valtwo", "three": {"four": "valfour", "five": "valfive"}}';
+
+const result = [
+  { key: 'one', value: 'valone' },
+  { key: 'two', value: 'valtwo' },
+  { key: 'three',
+    value: [
+    { key: 'four', value: 'valfour' },
+    { key: 'five', value: 'valfive' },
+    ] },
+];
+
+test('json decode test', () => {
+  // expect(func(json)).equalTo(result);
+});
--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -7,14 +7,13 @@ import print from 'mylittlecolor';
 
 commander
   .version('0.1.0')
-  .option('-f, --format [type]', 'Output format');
+  .option('-f, --format [type]', 'Output format', 'json');
 
 
 commander
   .arguments('<first_config> <second_config>')
   .action((firstConfig, secondConfig) => {
     // passing arguments
-    print(`#red{${firstConfig}} #green{${secondConfig}}`);
   });
 
 commander
